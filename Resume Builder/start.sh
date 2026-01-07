914a77332678cbb908cc1b92bf49b86d38fcddc6
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2026-01-06 20:05:29 -0500

    updates

--- Resume Builder/start.sh
+++ Resume Builder/start.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-set -e
 
 echo "Waiting for Postgres..."
-
 until pg_isready -h resume-postgres -p 5432 -U postgres; do
   sleep 1
 done
 
 echo "Postgres is ready. Running migrations..."
-
+cd /app
 alembic upgrade head
 
-echo "Starting FastAPI..."
+echo "Starting API..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
