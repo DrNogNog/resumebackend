74adde41a9a2b3df055a5088ddf141aadc2cf374
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2026-01-06 21:54:18 -0500

    removed .

--- Resume Builder/app/main.py
+++ Resume Builder/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.middleware.cors import CORSMiddleware.
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import os
 
