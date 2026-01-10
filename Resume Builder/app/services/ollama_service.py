603b7fe5e88db6d912c3e1fb655cd42e3ced6fbc
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2026-01-10 16:52:05 -0500

    upd

--- Resume Builder/app/services/ollama_service.py
+++ Resume Builder/app/services/ollama_service.py
@@ -21,10 +21,10 @@ async def call_ollama(prompt: str, model: str = DEFAULT_MODEL) -> str:
         return data["choices"][0]["text"]
 
 async def generate_resume_prompt(resume: str, job_description: str) -> str:
-    return f"Given this f{resume} out of 100 show me the match percentage between this resume and job description. Only output a score and a summary  of if the candidate has soft, strong or medium alignment to core technical requirements, list these requirements out. Note if the years of experience matches the job description. Note any soft skills the candidate should focus on based on the job description. Do this under 100 words or less. {job_description}"
+    return f"Given this {resume} out of 100 show me the match percentage between this resume and job description. Only output a score and a summary  of if the candidate has soft, strong or medium alignment to core technical requirements, list these requirements out. Note if the years of experience matches the job description. Note any soft skills the candidate should focus on based on the job description. Do this under 100 words or less. {job_description}"
 
 async def generate_sw_prompt(resume: str, job_description: str) -> str:
-    return f"Given this f{resume} give me the strengths and weaknesses of this resume. Do this under 100 words or less. Format: Strengths: [List of Strengths] Weaknesses: [List of Weaknesses] {job_description}"
+    return f"Given this {resume} give me the strengths and weaknesses of this resume. Do this under 100 words or less. Format: Strengths: [List of Strengths] Weaknesses: [List of Weaknesses] {job_description}"
 
 async def generate_match_prompt(resume: str, job_description:str) -> str:
-    return f"Given this f{resume} give me 3 specific tips to improve the resume to better match the and for each tip generate 3 resume bullet points in xyz format. Do this under 100 words or less. So give me 3 tips in one sentence, and three resume bullet points {job_description}"
+    return f"Given this {resume} give me 3 specific tips to improve the resume to better match the and for each tip generate 3 resume bullet points in xyz format. Do this under 100 words or less. So give me 3 tips in one sentence, and three resume bullet points {job_description}"
